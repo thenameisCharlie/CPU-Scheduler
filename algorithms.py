d177188dd8a57d61c77507c7bce00319b2c48d98
Charlie <143125248+CarlosVillatoro1995@users.noreply.github.com>
2025-02-23 18:37:12 -0500

    optimized FCFS algorithm and added CPU util and calculation functions

--- algorithms.py
+++ algorithms.py
@@ -21,37 +21,95 @@ if algorithmSelection == "1":
 
         ready_queue = deque(process_data.keys())
         io_queue = [] #min-heap queue will track I/O to ensure first in first out
-        systemCurrentTime = 0
+        systemCurrentTime = 0 
+        executionOrder = [] #This list will store the order in which the processes are executed 
+
+        completionTimes = {} #This dictionary will store the completion time for each process (will be used for calculating turnaround time)
+        firstCPUTime = {} #This dictionary will store the first CPU time for each process (will be used for calculating response time)
+        totalCPUTime = {p: sum(process_data[p][::2]) for p in process_data} #This dictionary will store the total CPU time for each process (will be used for calculating CPU utilization/waiting times)
 
         while ready_queue or io_queue:
+
+            # Display context switch details
+            print("\n🔹 Context Switch:")
+            print(f"🔹 Current Execution Time: {systemCurrentTime}")
+            print(f"🔹 Ready Queue: {[(p, process_data[p][0]) for p in ready_queue]}")  # Show next CPU bursts
+            print(f"🔹 I/O Queue: {[(p, t) for t, p in io_queue]}")  # Show remaining I/O time
+
             if ready_queue:
                 current_process = ready_queue.popleft() #get the first process in the queue
                 bursts = process_data[current_process] #get the burst times for the current process
-                print("Current Process: ", current_process)
+                executionOrder.append(current_process) 
+
+                if current_process not in firstCPUTime:
+                    firstCPUTime[current_process] = systemCurrentTime
 
                 if bursts:
                     cpu_burst = bursts.pop(0) #Runs the process for the CPU burst time
-                    print(f"Time {systemCurrentTime}: {current_process} running for {cpu_burst} units")
-                    systemCurrentTime += cpu_burst #increment the current time by the CPU burst time
-                    print(f"Time {systemCurrentTime}: {current_process} finished CPU burst")
+                    print(f"🔹 Running Process: {current_process} (CPU Burst: {cpu_burst})")
+                    systemCurrentTime += cpu_burst #increment the current time by the CPU burst time     
+                    print(f"🔹 Time {systemCurrentTime}: {current_process} finished CPU burst")          
 
                     if bursts:
                         io_burst = bursts.pop(0)
                         completionTime = systemCurrentTime + io_burst #total time a process waits and executes
-                        heapq.heappush(io_queue, (completionTime, current_process)) #using a min-heap to track I/O burst
-                        print(f"Time {systemCurrentTime}: {current_process} performing I/O for {io_burst} units")
+                        heapq.heappush(io_queue, (completionTime, current_process)) #using a min-heap to track I/O burst using tuple (completionTime, process)
+                        print(f"🔹 Time {systemCurrentTime}: {current_process} performing I/O for {io_burst} units")
+                
             
-            #If ready queue is empty, jump to the next I/O completion time
-            if not ready_queue and io_queue:
-                systemCurrentTime = io_queue[0][0]
+          
 
-            #Check if the I/O queue is not empty and the first process in the queue has a completion time less than or equal to the current time
+            #Check if the I/O queue is not empty and the first process in the queue has a completion time less than or equal to the current time to return to the ready queue
             while io_queue and io_queue[0][0] <= systemCurrentTime:
                 completionTime, current_process = heapq.heappop(io_queue) #pop the first process in the I/O queue and assign it to the current process
-                systemCurrentTime = completionTime #update the current time to the completion time 
-                ready_queue.append(current_process)
-                print(f"Time {systemCurrentTime}: {current_process} finished I/O")
-            
+                systemCurrentTime = completionTime
+                ready_queue.append(current_process) 
+                print(f"🔹 Time {systemCurrentTime}: {current_process} finished I/O and moved back to Ready Queue")
+
+        for process in executionOrder:
+            completionTimes[process] = systemCurrentTime
+
+        return executionOrder, completionTimes, firstCPUTime, totalCPUTime, systemCurrentTime
             
     
-    print(FCFS(process_data))
+    #function that computes Ttr, Tw, Tr for each process
+    def calculate_metrics(completionTimes, firstCPUTime, totalCPUTime):
+        turnaroundTimes = {}
+        waitingTimes = {}
+        responseTimes = {}
+
+        totalProcesses = len(completionTimes)
+
+        totalTurnaroundTime = 0
+        totalWaitingTime = 0
+        totalResponseTime = 0
+
+        # Calculate Ttr, Tw, Tr for each process
+        for process in completionTimes:
+            Ttr = completionTimes[process]
+            Tw = Ttr - totalCPUTime[process]
+            Tr = firstCPUTime[process]
+
+            turnaroundTimes[process] = Ttr
+            waitingTimes[process] = Tw
+            responseTimes[process] = Tr
+
+            totalTurnaroundTime += Ttr
+            totalWaitingTime += Tw
+            totalResponseTime += Tr
+
+        avgTurnaroundTime = totalTurnaroundTime / totalProcesses
+        avgWaitingTime = totalWaitingTime / totalProcesses
+        avgResponseTime = totalResponseTime / totalProcesses
+
+        return turnaroundTimes, waitingTimes, responseTimes, avgTurnaroundTime, avgWaitingTime, avgResponseTime
+
+
+
+    #Function that calculates the CPU utilization 
+    def calculate_cpu_utilization(totalCPUTime, systemCurrentTime):
+        
+        totalCPUExecutionTime = sum(totalCPUTime.values())  # Sum of all CPU bursts
+        cpuUtilization = (totalCPUExecutionTime / systemCurrentTime) * 100
+        
+        return cpuUtilization
